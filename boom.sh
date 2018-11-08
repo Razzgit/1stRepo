04945d4051490e6478d61b3bcb359e35613aed34
root <root@ip-172-31-2-1.ap-south-1.compute.internal>
2018-11-08 10:19:02 +0530

    WEBHOOKS TESTING

--- boom.sh
+++ boom.sh
@@ -2,5 +2,7 @@ sudo apt-get update
 sudo apt-get install apache2 -y
 sudo service apache2 start
 sudo netstat -uplnt
-sudo echo "Welocme to Raj's WorkPlace" > /var/www/html/index.html
+echo ""
+echo ""
+sudo echo "Welocme to Raj's WorkPlace and TESSTING WEBHOOKS ON SLAVE NODES" > /var/www/html/index.html
 sudo service apache2 restart
